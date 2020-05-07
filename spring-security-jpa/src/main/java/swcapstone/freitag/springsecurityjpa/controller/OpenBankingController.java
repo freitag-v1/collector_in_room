e32f590b745f3194f7a56dc99ffa03a659ed4dfc
최재웅 <wodnd999999@ajou.ac.kr>
2020-05-08 08:32:20 +0900

    Refactoring

--- spring-security-jpa/src/main/java/swcapstone/freitag/springsecurityjpa/controller/OpenBankingController.java
+++ spring-security-jpa/src/main/java/swcapstone/freitag/springsecurityjpa/controller/OpenBankingController.java
@@ -2,54 +2,32 @@ package swcapstone.freitag.springsecurityjpa.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import swcapstone.freitag.springsecurityjpa.externalAPI.APICaller;
+import swcapstone.freitag.springsecurityjpa.externalAPI.OpenBanking;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 
 @RestController
 public class OpenBankingController {
-    @RequestMapping("/externalapi/authorizetoken")
-    public void autorizetoken(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        System.out.println("==========authorize==========");
-        Map<String, String[]> parametes = request.getParameterMap();
-        Iterator<String> keys = parametes.keySet().iterator();
-        while(keys.hasNext()) {
-            String key = keys.next();
-            System.out.println(key + ":");
-            for(int i = 0; i < parametes.get(key).length; i++) {
-                System.out.println(parametes.get(key)[i]);
+    @RequestMapping("/externalapi/openbanking/oauth/token")
+    public void autorizetoken(HttpServletRequest request, HttpServletResponse response)  {
+        if(isAuthorizeToken(request)) {
+            String authorizeToken = request.getParameter("code");
+            String state = request.getParameter("state");
+
+            try {
+                Map<String, String> result = OpenBanking.getInstance().getAccessToken(authorizeToken);
+                // DB에서 state가 동일한 user를 찾아서 result.get("access_token"), result.get("user_seq_no") 저장
+                System.out.println("access_token:" + result.get("access_token"));
+                System.out.println("user_seq_no:" + result.get("user_seq_no"));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }
-        APICaller accessToken = new APICaller("POST", "https://testapi.openbanking.or.kr/oauth/2.0/token");
-        accessToken.setHeader("content-type", "application/x-www-form-urlencoded");
-        accessToken.setFiled("code", parametes.get("code")[0]);
-        accessToken.setFiled("client_id", "4wbVEVJf0Iuj9ckTMvO9uNMVg9kdJq5kJEZ7QiHS");
-        accessToken.setFiled("client_secret", "lzp4N3ao3vRdpKZRW2AxtPDVv496ZV7DQQAkNNp8");
-        accessToken.setFiled("redirect_uri", "http://wodnd999999.iptime.org:8080/externalapi/authorizetoken");
-        accessToken.setFiled("grant_type", "authorization_code");
-        try {
-            System.out.println(accessToken.getResponse());
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
-    //다르면 안됨...
-    /*@RequestMapping("/externalapi/accesstoken")
-    public void accesstoken(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        System.out.println("==========access==========");
-        Map<String, String[]> parametes = request.getParameterMap();
-        Iterator<String> keys = parametes.keySet().iterator();
-        while(keys.hasNext()) {
-            String key = keys.next();
-            for(int i = 0; i < parametes.get(key).length; i++) {
-                System.out.println(parametes.get(key)[i]);
-            }
-        }
-    }*/
+    private boolean isAuthorizeToken(HttpServletRequest request) {
+        return !request.getParameterMap().isEmpty();
+    }
 }
--- spring-security-jpa/src/main/java/swcapstone/freitag/springsecurityjpa/externalAPI/OpenBanking.java
+++ spring-security-jpa/src/main/java/swcapstone/freitag/springsecurityjpa/externalAPI/OpenBanking.java
@@ -4,18 +4,21 @@ import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class OpenBanking {
     private static OpenBanking instance = null;
-    private static final String clientID = "4wbVEVJf0Iuj9ckTMvO9uNMVg9kdJq5kJEZ7QiHS";
-    private static final String clientSecret = "lzp4N3ao3vRdpKZRW2AxtPDVv496ZV7DQQAkNNp8";
+    private static final String clientID = "XXyvh2Ij7l9rss0HAVObS880qY3penX57JXkib9q";
+    private static final String clientSecret = "p947iEbMvXjslhyTw4d4p2HK8U0DkOHR7o83Mtdx";
     private static final String baseURL = "https://testapi.openbanking.or.kr";
-    private String accessToken;
-    private String clientCode;
+    private static final String callbackURL = "http://wodnd999999.iptime.org:8080/externalapi/openbanking/oauth/token";
+    //private String accessToken;
+    private static String clientCode = "T991627830";
 
-    private OpenBanking() throws Exception {
-        APICaller getAccessToken = new APICaller("POST", baseURL + "/oauth/2.0/token");
+    private OpenBanking() {
+        /*APICaller getAccessToken = new APICaller("POST", baseURL + "/oauth/2.0/token");
         getAccessToken.setQueryParameter("client_id", clientID);
         getAccessToken.setQueryParameter("client_secret", clientSecret);
         getAccessToken.setQueryParameter("grant_type", "client_credentials");
@@ -24,21 +27,35 @@ public class OpenBanking {
         String response = getAccessToken.getResponse();
         JSONObject jResponse = new JSONObject(response);
         this.accessToken = jResponse.get("access_token").toString();
-        this.clientCode = jResponse.get("client_use_code").toString();
+        this.clientCode = jResponse.get("client_use_code").toString();*/
     }
 
     public static OpenBanking getInstance() {
         if(instance == null) {
-            try {
-                instance = new OpenBanking();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            instance = new OpenBanking();
         }
         return instance;
     }
 
-    public String getRealName(int userBank, String userAccount, int userBirthday) throws Exception {
+    public Map<String, String> getAccessToken(String authorizeToken) throws Exception {
+        APICaller getAccessToken = new APICaller("POST", baseURL + "/oauth/2.0/token");
+        getAccessToken.setHeader("content-type", "application/x-www-form-urlencoded");
+        getAccessToken.setFiled("code", authorizeToken);
+        getAccessToken.setFiled("client_id", clientID);
+        getAccessToken.setFiled("client_secret", clientSecret);
+        getAccessToken.setFiled("redirect_uri", callbackURL);
+        getAccessToken.setFiled("grant_type", "authorization_code");
+
+        String response = getAccessToken.getResponse();
+        JSONObject jResponse = new JSONObject(response);
+        Map<String, String> result = new HashMap<>();
+        result.put("access_token", jResponse.get("access_token").toString());
+        result.put("user_seq_no", jResponse.get("user_seq_no").toString());
+
+        return result;
+    }
+
+    /*public String getRealName(int userBank, String userAccount, int userBirthday) throws Exception {
         APICaller getRealName = new APICaller("POST", baseURL + "/v2.0/inquiry/real_name");
         getRealName.setHeader("Authorization", "Bearer " + accessToken);
         getRealName.setJsonBody("bank_tran_id", getTransactionID());
@@ -51,7 +68,7 @@ public class OpenBanking {
         String response = getRealName.getResponse();
         JSONObject jResponse = new JSONObject(response);
         return jResponse.get("account_holder_name").toString();
-    }
+    }*/
 
     private String getTransactionID() {
         String uniqueCode = new SimpleDateFormat("HHmmssSSS", Locale.KOREA).format(new Date());
