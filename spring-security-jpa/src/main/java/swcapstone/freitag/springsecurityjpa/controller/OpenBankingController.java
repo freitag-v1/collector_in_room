5285ae497f13dfac19bbc8a0f862a1c143ba4ab5
최재웅 <wodnd999999@ajou.ac.kr>
2020-05-11 05:09:52 +0900

    오픈뱅킹 등록 구현 완료

--- spring-security-jpa/src/main/java/swcapstone/freitag/springsecurityjpa/controller/OpenBankingController.java
+++ spring-security-jpa/src/main/java/swcapstone/freitag/springsecurityjpa/controller/OpenBankingController.java
@@ -1,6 +1,7 @@
 package swcapstone.freitag.springsecurityjpa.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import swcapstone.freitag.springsecurityjpa.domain.UserEntity;
@@ -19,6 +20,7 @@ public class OpenBankingController {
     private UserRepository userRepository;
 
     @RequestMapping("/externalapi/openbanking/oauth/token")
+    @Transactional
     public void autorizetoken(HttpServletRequest request, HttpServletResponse response)  {
         if(hasAuthorizeToken(request)) {
             String authorizeToken = request.getParameter("code");
@@ -30,6 +32,8 @@ public class OpenBankingController {
                 // DB에서 state가 동일한 user를 찾아서 access_token과 user_seq_no 저장
                 Optional<UserEntity> userEntityWrapper = userRepository.findByUserOpenBankingAccessToken(state);
                 userEntityWrapper.ifPresent(selectUser -> {
+                    System.out.println("해당하는 유저 찾음.");
+                    System.out.println("access token, user_seq_no 저장");
                     selectUser.setUserOpenBankingAccessToken(result.get("access_token"));
                     selectUser.setUserOpenBankingNum(Integer.parseInt(result.get("user_seq_no")));
                 });
